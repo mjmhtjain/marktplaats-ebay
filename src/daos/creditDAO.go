224f561d07a74ef4c95799b8a6ed895003ec3a7a
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2022-01-02 15:50:03 +0100

    implementing CreditorDAO and modelling changes

--- src/daos/creditDAO.go
+++ src/daos/creditDAO.go
@@ -1,6 +1,14 @@
 package daos
 
-import "github.com/mjmhtjain/marktplaats-ebay/src/models"
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/mjmhtjain/marktplaats-ebay/src/models"
+	"github.com/mjmhtjain/marktplaats-ebay/src/setup"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type CreditDAO interface {
 	GetAll() ([]models.Creditor, error)
@@ -8,16 +16,41 @@ type CreditDAO interface {
 }
 
 type ecgCreditDAO struct {
+	client     *mongo.Client
+	collection *mongo.Collection
 }
 
 func NewECGCreditDAO() CreditDAO {
-	return &ecgCreditDAO{}
+	c := setup.NewMongo().Client
+	creditorsCollection := c.Database("ecg").Collection("creditors")
+
+	return &ecgCreditDAO{
+		client:     c,
+		collection: creditorsCollection,
+	}
 }
 
 func (dao *ecgCreditDAO) GetAll() ([]models.Creditor, error) {
 	return nil, nil
 }
 
-func (dao *ecgCreditDAO) InsertAll([]models.Creditor) ([]models.Creditor, error) {
-	return nil, nil
+func (dao *ecgCreditDAO) InsertAll(creditors []models.Creditor) ([]models.Creditor, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	arr := []interface{}{}
+	for _, c := range creditors {
+		arr = append(arr, c)
+	}
+
+	res, err := dao.collection.InsertMany(ctx, arr)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(res.InsertedIDs) > 0 {
+		return creditors, nil
+	}
+
+	return nil, errors.New("Something")
 }
--- src/models/Creditor.go
+++ src/models/Creditor.go
@@ -1,10 +1,13 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Creditor struct {
-	Name     string  `csv:"Name"`
-	Address  string  `csv:"Address"`
-	Postcode string  `csv:"Postcode"`
-	Phone    string  `csv:"Phone"`
-	Credit   float64 `csv:"Credit Limit"`
-	Birthday string  `csv:"Birthday"`
+	ID       primitive.ObjectID `json:"-" bson:"_id,omitempty"`
+	Name     string             `json:"name" csv:"Name" bson:"name"`
+	Address  string             `json:"address" csv:"Address" bson:"address"`
+	Postcode string             `json:"postcode" csv:"Postcode" bson:"postcode"`
+	Phone    string             `json:"phone" csv:"Phone" bson:"phone"`
+	Credit   float64            `json:"credit" csv:"Credit Limit" bson:"credit"`
+	Birthday string             `json:"birthday" csv:"Birthday" bson:"birthday"`
 }
